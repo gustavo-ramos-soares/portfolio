6817c24126e9a35c1eeab1abad59021779f6ab46
Gustavo Ramos <guhramos315@hotmail.com>
2026-06-07 10:58:23 -0300

    ajustes gerais de layout, tema e tipografia no portfólio

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+// Dark Theme
+const btn = document.getElementById('btnTema');
+const icon = document.getElementById('themeIcon');
+
+function trocarTema() {
+    document.body.classList.toggle('light');
+
+    if (document.body.classList.contains('light')) {
+        icon.className = "fa-solid fa-sun";
+    } else {
+        icon.className = "fa-solid fa-moon";
+    }
+}
+
+btn.addEventListener("click", trocarTema);
+
+
+// Ano
+let anoAtual = document.getElementById('anoAtual')
+anoAtual.textContent = new Date().getFullYear()
+
